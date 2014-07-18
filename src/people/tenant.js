bb00de91044efcb659517013e8fc99ae3f3d044c
smonette <steph@stephmonette.com>
2014-07-18 11:00:20 -0700

    starting persons file

--- src/people/tenant.js
+++ src/people/tenant.js
@@ -2,11 +2,14 @@ var person = require("./person.js");
 
 function Tenant(name, contact) {
   // set this.name, and contact
+  this.name = name;
+  this.contact = contact;
   this.references = [];
 };
 
 Tenant.prototype.addReference = function(reference){
   // add reference to references
+   this.references.push(reference);
 };
 
 module.exports = Tenant;
